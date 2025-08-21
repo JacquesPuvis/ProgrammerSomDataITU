bd1b88727765e6079c74c753e7fde8e83805490a
JacquesPuvis <japu@itu.dk>
2025-08-21 13:11:07 +0200

    prog

--- Assignments/Ass1/Program.fs
+++ Assignments/Ass1/Program.fs
@@ -31,9 +31,14 @@ let e2 = Prim("+", CstI 3, Var "a");;
 
 let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
 
+let e4 = Prim ("==", CstI 17, e1);;
+
+let e5 = Prim ("max", e1, e2);;
+let e6 = Prim ("min", e1, e2);;
+
 
 (* Evaluation within an environment *)
-
+//(i)
 let rec eval e (env : (string * int) list) : int =
     match e with
     | CstI i            -> i
@@ -41,9 +46,35 @@ let rec eval e (env : (string * int) list) : int =
     | Prim("+", e1, e2) -> eval e1 env + eval e2 env
     | Prim("*", e1, e2) -> eval e1 env * eval e2 env
     | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim _            -> failwith "unknown primitive";;
+    | Prim ("==", e1,e2) -> if eval e1 env = eval e2 env then 1 else 0
+    | Prim ("max", e1,e2) -> if eval e1 env < eval e2 env then eval e2 env else eval e1 env
+    | Prim ("min", e1,e2) -> if eval e1 env < eval e2 env then eval e1 env else eval e2 env
+
+
+
+
+//(iii)
+let rec eval2 e (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x 
+    | Prim(ope, e1, e2) -> 
+        let i1 = eval2 e1 env
+        let i2 = eval e2 env
+        match ope with
+        | "+" -> i1 + i2
+        | "*" -> i1 * i2
+        | "-" -> i1 - i2
+        | "==" -> if i1 = i2 then 1 else 0
+        | "max" -> if i1 < i2 then i2 else i1
+        | "min" -> if i1 < i2 then i1 else i2
 
 let e1v  = eval e1 env;;
 let e2v1 = eval e2 env;;
 let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env;;
+
+//(ii)
+let e4v  = eval e4 env;;
+let e5v  = eval e5 env;;
+let e6v  = eval e6 env;;
