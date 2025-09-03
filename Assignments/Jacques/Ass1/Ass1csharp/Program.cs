cc457ddbc851121f4fd35d85a27005f551068cd1
JacquesPuvis <japu@itu.dk>
2025-09-03 14:12:48 +0200

    remove

--- Assignments/Jacques/Ass1/Ass1csharp/Program.cs
+++ Assignments/Jacques/Ass1/Ass1csharp/Program.cs
@@ -24,14 +24,12 @@ Console.WriteLine("Evaluation: " + g.Eval(env));
 Console.WriteLine("Simplified: " + g.Simplify());
 Console.WriteLine();
 
-// More complex example
 Expr complex = new Mul(new Var("a"), new Add(new Var("b"), new CstI(3)));
 Console.WriteLine("Expression: " + complex);
 Console.WriteLine("Evaluation: " + complex.Eval(env));
 Console.WriteLine("Simplified: " + complex.Simplify());
 Console.WriteLine();
 
-// Examples that show simplification rules
 Console.WriteLine("=== Simplification Examples ===");
 Expr addZero = new Add(new Var("x"), new CstI(0));
 Console.WriteLine("x + 0 = " + addZero.Simplify());
@@ -45,6 +43,5 @@ Console.WriteLine("x * 0 = " + mulZero.Simplify());
 Expr subZero = new Sub(new Var("x"), new CstI(0));
 Console.WriteLine("x - 0 = " + subZero.Simplify());
 
-// Nested simplification
 Expr nested = new Add(new Mul(new CstI(0), new Var("x")), new Mul(new CstI(1), new Var("y")));
 Console.WriteLine("(0 * x) + (1 * y) = " + nested.Simplify());
